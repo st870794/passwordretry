b020f504a7e5aaf6258f30c673c0f6f974350b3a
Jia Ren <st870794@gmail.com>
2020-08-10 20:21:51 +0800

    change to i > 0

--- passwordretry.py
+++ passwordretry.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 i = 3 # 剩餘機會
-while True:
+while i > 0:
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('登入成功!')
@@ -11,4 +11,3 @@ while True:
 		print('密碼錯誤!還有', i, '次機會')
 		if i == 0:
 			print('已經暫時封鎖10分鐘')
-			break
